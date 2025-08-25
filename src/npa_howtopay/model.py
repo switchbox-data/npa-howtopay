cf4d3b981fa6142ccd3d250bec331fb7eaf79c39
alxsmith <aedwards.smith@gmail.com>
2025-08-25 15:07:37 +0000

    inital framework - need to define input params to check logic

--- src/npa_howtopay/model.py
+++ src/npa_howtopay/model.py
@@ -1,12 +1,7 @@
 import polars as pl
 from params import InputParams, ScenarioParams
 
-from capex_project import (
-    grid_upgrade_capex_projects,
-    lpp_gas_capex_projects,
-    non_lpp_gas_capex_projects,
-    non_npa_electric_capex_projects,
-)
+import capex_project as cp
 
 
 def compute_blue_columns(year: int, gas_params: GasParams, shared_params: SharedParams) -> pl.DataFrame:
@@ -16,6 +11,9 @@ def compute_blue_columns(year: int, gas_params: GasParams, shared_params: Shared
 
 
 def run_model(scenario_params: ScenarioParams, input_params: InputParams, npa_projects: pl.DataFrame) -> pl.DataFrame:
+    gas_ratebase = input_params.gas_ratebase_init
+    electric_ratebase = input_params.electric_ratebase_init
+
     gas_capex_projects = pl.DataFrame()
     electric_capex_projects = pl.DataFrame()
 
@@ -25,42 +23,85 @@ def run_model(scenario_params: ScenarioParams, input_params: InputParams, npa_pr
 
     for year in range(scenario_params.start_year, scenario_params.end_year):
         # INFLATION - probably looks like evolving input_params to current_params
-        live_params = do_cost_inflation(year, live_params)
+        live_params = do_cost_inflation(year, live_params)  # TODO
 
         # get the npas for this year
         npas_this_year = npa_projects.filter(pl.col("year") == year)
 
-        ####### ticket
         # gas capex
-        gas_capex_projects = gas_capex_projects.vstack(
-            non_lpp_gas_capex_projects(year, input_params)
-        )  # maybe has different depreciation schedule
-        gas_capex_projects = gas_capex_projects.vstack(lpp_gas_capex_projects(year, input_params, npas_this_year))
+        gas_capex_projects = pl.concat(
+            [
+                gas_capex_projects,
+                cp.get_synthetic_initial_capex_projects(
+                    year=year,
+                    initial_ratebase=input_params.gas_ratebase,
+                    depreciation_lifetime=input_params.gas_depreciation_lifetime,
+                ),
+                cp.get_non_lpp_gas_capex_projects(
+                    year=year,
+                    current_ratebase=gas_ratebase,
+                    baseline_non_lpp_gas_ratebase_growth=input_params.gas_ratebase_growth,
+                    depreciation_lifetime=input_params.gas_depreciation_lifetime,
+                ),
+                cp.get_lpp_gas_capex_projects(
+                    year=year,
+                    gas_bau_lpp_costs_per_year=input_params.gas_bau_lpp_costs_per_year,
+                    npas_this_year=npas_this_year,
+                    depreciation_lifetime=input_params.gas_depreciation_lifetime,
+                ),
+            ],
+            how="vertical",
+        )
 
-        ####### ticket
         # electric capex
-        electric_capex_projects = electric_capex_projects.vstack(non_npa_electric_capex_projects(year, input_params))
-        electric_capex_projects = electric_capex_projects.vstack(
-            grid_upgrade_capex_projects(year, input_params, npa_projects)
+        electric_capex_projects = pl.concat(
+            [
+                electric_capex_projects,
+                cp.get_non_npa_electric_capex_projects(
+                    year=year,
+                    current_ratebase=electric_ratebase,
+                    baseline_electric_ratebase_growth=input_params.electric_ratebase_growth,
+                    depreciation_lifetime=input_params.electric_depreciation_lifetime,
+                ),
+                cp.get_grid_upgrade_capex_projects(
+                    year=year,
+                    npas_this_year=npas_this_year,
+                    peak_hp_kw=input_params.peak_hp_kw,
+                    peak_aircon_kw=input_params.peak_aircon_kw,
+                    distribution_cost_per_peak_kw_increase=input_params.distribution_cost_per_peak_kw_increase,
+                    grid_upgrade_depreciation_lifetime=input_params.grid_upgrade_depreciation_lifetime,
+                ),
+            ],
+            how="vertical",
         )
 
         # npa capex
         if scenario_params.capex_opex == "capex":
-            npa_capex = get_npa_capex_projects(npas_this_year, year, input_params)
+            npa_capex = cp.get_npa_capex_projects(
+                npas_this_year, year, input_params.npa_install_cost, input_params.npa_lifetime
+            )
             if scenario_params.gas_electric == "gas":
                 gas_capex_projects = gas_capex_projects.vstack(npa_capex)
             elif scenario_params.gas_electric == "electric":
                 electric_capex_projects = electric_capex_projects.vstack(npa_capex)
 
-        gas_ratebase = get_ratebase_from_capex_projects(year, gas_capex_projects)
-        electric_ratebase = get_ratebase_from_capex_projects(year, electric_capex_projects)
+        gas_ratebase = cp.compute_ratebase_from_capex_projects(year, gas_capex_projects)
+        electric_ratebase = cp.compute_ratebase_from_capex_projects(year, electric_capex_projects)
 
         ## TODO: opex stuff?
 
         ####### ticket
-        intermediate_columns = compute_blue_columns()  # returns pl.Series
+        intermediate_columns = compute_blue_columns()  # returns pl.DataFrame
 
-        output_df.append([gas_ratebase, electric_ratebase, intermediate_columns])  # bad syntax
+        # Build output row for this year
+        year_output = pl.DataFrame({
+            "year": [year],
+            "gas_ratebase": [gas_ratebase],
+            "electric_ratebase": [electric_ratebase],
+            # Add other columns as needed
+        })
+
+        output_df = pl.concat([output_df, year_output], how="vertical")
 
     output_df = compute_bill_costs(output_df, discount_rate)  # appends new columns to output_df
 
