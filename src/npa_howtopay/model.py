4fcc68d3602947e2dbd7b1b53a139855e27f439f
alxsmith <aedwards.smith@gmail.com>
2025-08-28 16:27:14 -0400

    remove final concat -function already appends and move __main__ to sample run script

--- src/npa_howtopay/model.py
+++ src/npa_howtopay/model.py
@@ -431,7 +431,7 @@ def run_model(scenario_params: ScenarioParams, input_params: InputParams, ts_par
         output_df = pl.concat([output_df, year_output], how="vertical")
 
     # appends new columns to output_df
-    results_df = pl.concat([output_df, compute_bill_costs(output_df, input_params)], how="vertical")
+    results_df = compute_bill_costs(output_df, input_params)
 
     return results_df
 
@@ -440,45 +440,3 @@ def run_model(scenario_params: ScenarioParams, input_params: InputParams, ts_par
 def analyze_scenarios(scenario_runs: dict[ScenarioParams, pl.DataFrame]) -> None:
     pass
 
-
-if __name__ == "__main__":
-    scenario_params = ScenarioParams(start_year=2025, end_year=2050, gas_electric="gas", capex_opex="capex")
-    input_params = load_scenario_from_yaml("sample")
-    ts_params = TimeSeriesParams(
-        npa_projects=pl.concat(
-            [
-                npa.generate_npa_projects(
-                    start_year=scenario_params.start_year,
-                    end_year=scenario_params.end_year,
-                    total_num_projects=10,
-                    num_converts_per_project=5,
-                    pipe_value_per_user=1000.0,
-                    pipe_decomm_cost_per_user=100.0,
-                    peak_kw_winter_headroom_per_project=10.0,
-                    peak_kw_summer_headroom_per_project=10.0,
-                    aircon_percent_adoption_pre_npa=0.8,
-                    pipe_decomm_cost_inflation_rate=input_params.shared.cost_inflation_rate,
-                ),
-                npa.generate_scattershot_electrification_projects(
-                    start_year=scenario_params.start_year,
-                    end_year=scenario_params.end_year,
-                    total_num_converts=10,
-                ),
-            ],
-            how="vertical",
-        ).sort("project_year"),
-        gas_bau_lpp_costs_per_year=pl.DataFrame({
-            "year": list(range(scenario_params.start_year, scenario_params.end_year + 1)),
-            "cost": [100] * (scenario_params.end_year - scenario_params.start_year + 1),
-        }).sort("year"),
-        electric_fixed_overhead_costs=pl.DataFrame({
-            "year": list(range(scenario_params.start_year, scenario_params.end_year + 1)),
-            "cost": [100] * (scenario_params.end_year - scenario_params.start_year + 1),
-        }).sort("year"),
-        gas_fixed_overhead_costs=pl.DataFrame({
-            "year": list(range(scenario_params.start_year, scenario_params.end_year + 1)),
-            "cost": [100] * (scenario_params.end_year - scenario_params.start_year + 1),
-        }).sort("year"),
-    )
-
-    run_model(scenario_params, input_params, ts_params)
