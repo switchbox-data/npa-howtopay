3526566dd7001ae781d7239dd32a4c7d4f20616c
alxsmith <aedwards.smith@gmail.com>
2025-09-09 15:16:35 -0400

    uncomment web parms

--- run_example.py
+++ run_example.py
@@ -4,7 +4,7 @@ import polars as pl
 
 from npa_howtopay.model import analyze_scenarios, create_scenario_runs
 from npa_howtopay.params import (
-    # load_time_series_params_from_web_params,
+    load_time_series_params_from_web_params,
     COMPARE_COLS,
     load_scenario_from_yaml,
     load_time_series_params_from_yaml,
@@ -67,24 +67,24 @@ if __name__ == "__main__":
     plot_total_bills(delta_bau_df, switchbox_colors, save_dir="plots")
 
 # Method 2: Using web parameters (scalar values)
-# web_params = {
-#     "npa_num_projects": 10,
-#     "num_converts": 100,
-#     "pipe_value_per_user": 1000.0,
-#     "pipe_decomm_cost_per_user": 100.0,
-#     "peak_kw_winter_headroom": 10.0,
-#     "peak_kw_summer_headroom": 10.0,
-#     "aircon_percent_adoption_pre_npa": 0.8,
-#     "non_npa_scattershot_electrifiction_users_per_year": 5,
-#     "gas_fixed_overhead_costs": 100.0,
-#     "electric_fixed_overhead_costs": 100.0,
-#     "gas_bau_lpp_costs_per_year": 100.0,
-#     "is_scattershot": False,
-# }
+web_params = {
+    "npa_num_projects": 10,
+    "num_converts": 100,
+    "pipe_value_per_user": 1000.0,
+    "pipe_decomm_cost_per_user": 100.0,
+    "peak_kw_winter_headroom": 10.0,
+    "peak_kw_summer_headroom": 10.0,
+    "aircon_percent_adoption_pre_npa": 0.8,
+    "non_npa_scattershot_electrifiction_users_per_year": 5,
+    "gas_fixed_overhead_costs": 100.0,
+    "electric_fixed_overhead_costs": 100.0,
+    "gas_bau_lpp_costs_per_year": 100.0,
+    "is_scattershot": False,
+}
 
-# input_params2 = load_scenario_from_yaml(
-#     "sample")  # Still load base params from YAML
-# ts_params2 = load_time_series_params_from_web_params(
-#     web_params, 2025, 2050)
+input_params2 = load_scenario_from_yaml(
+    "sample")  # Still load base params from YAML
+ts_params2 = load_time_series_params_from_web_params(
+    web_params, 2025, 2050)
 
-# out2 = analyze_scenarios(scenario_runs, input_params2, ts_params2)
+out2 = analyze_scenarios(scenario_runs, input_params2, ts_params2)
